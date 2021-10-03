b53abea7f90014f9f16aacf0db9935d2d6973382
Ivan Aronov <aron555@inbox.ru>
2021-10-03 23:46:44 +0300

    add md-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
-import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import LastDialogs from './components/lastDialogs/LastDialogs';
-import Profile from './components/profile/Profile';
-import Dialogs from './components/dialogs/Dialogs';
-import './App.css';
+import React from 'react'
+// import {Route} from 'react-router-dom'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import LastDialogs from './components/lastDialogs/LastDialogs'
+// import Profile from './components/profile/Profile'
+// import Dialogs from './components/dialogs/Dialogs'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
+
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+
 
 function App(props) {
 	return (
@@ -16,6 +24,8 @@ function App(props) {
 				<LastDialogs/>
 			</aside>
 			<div className="content">
+				<button className='btn btn-primary'><DoneAllIcon/> Примари <FontAwesomeIcon icon={faChevronDown}/>
+				</button>
 			</div>
 		</div>
 	);
